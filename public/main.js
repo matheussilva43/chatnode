28939a385ed15342a0666ef1078c5c4ba0b501ec
Matheus Pereira <msmatheuspereira@gmail.com>
2020-10-24 17:13:06 -0300

    Entrando no chat (2/2)

--- public/main.js
+++ public/main.js
@@ -6,10 +6,19 @@ let loginPage = document.querySelector('#loginPage');
 let chatPage = document.querySelector('#chatPage');
 
 let loginInput = document.querySelector('#loginNameInput');
-let textInput = document.querySelector('#chattextInput');
+let textInput = document.querySelector('#chatTextInput');
 
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
+
+function renderUserList() {
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = '';
+
+    userList.forEach(i => {
+        ul.innerHTML += '<li>'+i+'</li>'
+    });
+}
 
 loginInput.addEventListener('keyup', (e) => {
     if(e.keyCode === 13) {
@@ -17,6 +26,17 @@ loginInput.addEventListener('keyup', (e) => {
         if(name != '') {
             username = name;
             document.title = 'Chat ('+username+')';
+
+            socket.emit('join-request', username);
         }
     }
+});
+
+socket.on('user-ok', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+    textInput.focus();
+
+    userList = list;
+    renderUserList();
 });
--- server.js
+++ server.js
@@ -11,9 +11,17 @@ server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let connecteUsers = [];
+
 io.on('connection', (socket) => {
     console.log("Conexão detectada...");
 
-    
+socket.on('join-request', (username) => {
+    socket.username = username;
+    connecteUsers.push( username );
+    console.log( connecteUsers );
+
+    socket.emit('user-ok', connecteUsers);
+});
 
 });
